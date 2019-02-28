74f387e8d7b50877321bf50f11be901efd8afb7d
CathalRyan96 <cathalryan48@gmail.com>
2019-02-28 11:08:21 +0000

    added variables

--- sha256.c
+++ sha256.c
@@ -8,11 +8,20 @@ void sha256();
 
 int main(int argc, char *argv[]){
 
+  sha256();
 
   return 0;
 }
 
 void sha256(){
-
+  
+  //Message Schedule
+  uint32_t W[64];
+  //Working variables
+  uint32_t a, b, c, d, e, f, g;
+  // The Hash value.
+  uint32_t H[8];
+  //Temporary Variables
+  uint32_t T1, T2;
 
 }
