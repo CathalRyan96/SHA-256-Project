994173d83393402877bda59e6504d16d94214f6b
CathalRyan96 <cathalryan48@gmail.com>
2019-02-28 10:58:52 +0000

    added basic main method to sha256.c

---
+++ sha256.c
@@ -0,0 +1,18 @@
+// Cathal Ryan 2019
+// The Secure Hash Algorithm, 256 bit Version
+
+#include <stdio.h>
+#include <stdint.h>
+
+void sha256();
+
+int main(int argc, char *argv[]){
+
+
+  return 0;
+}
+
+void sha256(){
+
+
+}
