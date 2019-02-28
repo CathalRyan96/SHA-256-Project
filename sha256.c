232d43004c60ebf443ac603ae0a0c8c30917dd94
CathalRyan96 <cathalryan48@gmail.com>
2019-02-28 11:16:53 +0000

    added hash values

--- sha256.c
+++ sha256.c
@@ -17,11 +17,23 @@ void sha256(){
   
   //Message Schedule
   uint32_t W[64];
+
   //Working variables
   uint32_t a, b, c, d, e, f, g;
-  // The Hash value.
-  uint32_t H[8];
+  
   //Temporary Variables
   uint32_t T1, T2;
+
+  // The Hash value.
+  uint32_t H[8] = {
+     0x6a09e667
+   , 0xbb67ae85
+   , 0x3c6ef372
+   , 0xa54ff53a    
+   , 0x510e527f
+   , 0x9b05688c
+   , 0x1f83d9ab
+   , 0x5be0cd19
+  };
 
 }
